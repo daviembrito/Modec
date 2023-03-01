b5ce393cacb53be83d615a59b7e286602bf65867
Davi <daviibrito111@gmail.com>
2023-03-01 14:17:35 -0300

    Added from_number to constructor

--- twilioapp.py
+++ twilioapp.py
@@ -1,9 +1,9 @@
 from twilio.rest import Client
 
 class TwilioClient(Client):
-    def __init__(self, account_sid, token):
+    def __init__(self, account_sid, token, from_number):
         super().__init__(account_sid, token)
-        self.from_number = "+14155238886"
+        self.from_number = from_number
 
     def sendImage(self, to_number, image_url, title):
         return self.messages.create(
